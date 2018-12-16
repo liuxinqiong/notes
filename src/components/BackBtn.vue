b8a8826c7c1f11e72665aa31ff4724dc318442e4
fly <929755881@qq.com>
2018-12-16 23:46:23 +0800

    背景图,返回主页按钮组件

---
+++ src/components/BackBtn.vue
@@ -0,0 +1,28 @@
+<template>
+    <img class='back-btn' src='../assets/img/back.png' @click='goBack' />
+</template>
+
+<script>
+    export default {
+        methods: {
+            goBack: function () {
+                const url = "../index/main";
+                wx.navigateTo({
+                    url
+                });
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .back-btn {
+        width: 72rpx;
+        height: 72rpx;
+        position: absolute;
+        left: 53rpx;
+        top: 42rpx;
+    }
+
+</style>
