3983cb31ca7aa63f9f3899941efb55773dc03e7d
fanhao <alan.fan.h@qq.com>
2018-12-22 15:45:29 +0800

    add index.scss,edit index.vue,add components/img

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -65,83 +65,6 @@
         </div>
 </template>
 
-<script>
-    import card from '@/components/card'
-    import './index.scss'
-    import {
-        takePhoto,
-        showSuccess
-    } from '@/utils'
-    export default {
-        data() {
-            return {
-                motto: 'Hello World',
-                userInfo: {},
-                temp: []
-            }
-        },
-
-        components: {
-            card
-        },
-
-        methods: {
-            bindViewTap() {
-                const url = '../logs/main'
-                wx.navigateTo({
-                    url
-                })
-            },
-            getUserInfo() {
-                // 调用登录接口
-                wx.login({
-                    success: () => {
-                        wx.getUserInfo({
-                            success: (res) => {
-                                this.userInfo = res.userInfo
-                            }
-                        })
-                    }
-                })
-            },
-            clickHandle(msg, ev) {
-                console.log('clickHandle:', msg, ev)
-            },
-            async takePhote() {
-                this.temp = await takePhoto()
-            },
-            setCache() {
-                const fs = wx.getFileSystemManager()
-                fs.writeFileSync(`${wx.env.USER_DATA_PATH}/hello.txt`, 'hello, world', 'utf8')
-                // wx.setStorage({
-                //   key: 'age',
-                //   data: '20'
-                // })
-            },
-            getCache() {
-                const fs = wx.getFileSystemManager()
-                var res = fs.readFileSync(`${wx.env.USER_DATA_PATH}/hello.txt`, 'utf8')
-                showSuccess(res)
-                // wx.getStorage({
-                //   key: 'age',
-                //   success(res) {
-                //     showSuccess(res.data)
-                //   }
-                // })
-            }
-        },
-
-        created() {
-            // 调用应用实例的方法获取全局数据
-            this.getUserInfo()
-        },
-        mounted() {
-
-        }
-    }
-
-</script>
-
 <style lang="scss" scoped>
     .userinfo {
         display: flex;
@@ -178,42 +101,39 @@
         color: blue;
         border: 1px solid blue;
     }
-
-    // <form class="form-container"><input type="text"class="form-control"v-model="motto"placeholder="v-model"/><input type="text"class="form-control"v-model.lazy="motto"placeholder="v-model.lazy"/></form><a href="/pages/counter/main"class="counter">去往Vuex示例页面</a><a @click="takePhote">拍照</a><button @click="test">test</button></div></template><script>
+</style>
+// <form class="form-container"><input type="text" class="form-control" v-model="motto" placeholder="v-model" /><input
+        type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" /></form><a href="/pages/counter/main"
+    class="counter">去往Vuex示例页面</a><a @click="takePhote">拍照</a><button @click="test">test</button></div></template>
+<script>
     import card from "@/components/card";
-
+    import './index.scss'
     import {
-        takePhoto
-    }
-
-    from "@/utils";
+        takePhoto,
+        showSuccess
+    } from "@/utils";
 
     export default {
         data() {
             return {
-
                 motto: "Hello World",
                 userInfo: {}
             }
 
             ;
-        }
-
-        ,
+        },
 
         components: {
             card
-        }
-
-        ,
+        },
 
         methods: {
             bindViewTap() {
-                const url="../logs/main";
+                const url = "../logs/main";
 
-                wx.navigateTo( {
-                    url
-                }
+                wx.navigateTo({
+                        url
+                    }
 
                 );
             }
@@ -222,17 +142,15 @@
             getUserInfo() {
 
                 // 调用登录接口
-                wx.login( {
-                    success: ()=> {
-                        wx.getUserInfo( {
-                            success: res=> {
-                                this.userInfo=res.userInfo;
-                            }
+                wx.login({
+                        success: () => {
+                            wx.getUserInfo({
+                                success: res => {
+                                    this.userInfo = res.userInfo;
+                                }
+                            });
                         }
-
-                        );
                     }
-                }
 
                 );
             }
@@ -240,101 +158,46 @@
             ,
             clickHandle(msg, ev) {
                 console.log("clickHandle:", msg, ev);
-            }
-
-            ,
+            },
             async takePhote() {
                 try {
-                    const tempFilePath=await takePhoto();
+                    const tempFilePath = await takePhoto();
 
-                    console.log(tempFilePath) wx.navigateTo( {
-                        url: `/pages/cropper/main?src=$ {
-                            tempFilePath
+                    console.log(tempFilePath);
+                    wx.navigateTo({
+                            url: `/pages/cropper/main?src=$ {tempFilePath}`
                         }
 
-                        `
-                    }
-
                     )
-                }
-
-                catch(e) {
+                } catch (e) {
                     console.log(e)
                 }
-            }
-
-            ,
+            },
             test() {
-                wx.cloud.callFunction( {
+                wx.cloud.callFunction({
 
-                    // 云函数名称
-                    name: 'add',
-                    // 传给云函数的参数
-                    data: {
-                        a: 1,
+                        // 云函数名称
+                        name: 'add',
+                        // 传给云函数的参数
+                        data: {
+                            a: 1,
                             b: 2,
+                        },
+                        success(res) {
+                            console.log(res.result.sum) // 3
+                        },
+                        fail: console.error
                     }
-
-                    ,
-                    success(res) {
-                        console.log(res.result.sum) // 3
-                    }
-
-                    ,
-                    fail: console.error
-                }
 
                 )
             }
-        }
-
-        ,
+        },
 
         created() {
             // 调用应用实例的方法获取全局数据
             this.getUserInfo();
-        }
-
-        ,
+        },
         mounted() {}
     }
 
-    ;
-
-    </script><style lang="scss"scoped>.userinfo {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .userinfo-avatar {
-        width: 128rpx;
-        height: 128rpx;
-        margin: 20rpx;
-        border-radius: 50%;
-    }
-
-    .userinfo-nickname {
-        color: #aaa;
-    }
-
-    .usermotto {
-        margin-top: 150px;
-    }
-
-    .form-control {
-        display: block;
-        padding: 0 12px;
-        margin-bottom: 5px;
-        border: 1px solid #ccc;
-    }
-
-    .counter {
-        display: inline-block;
-        margin: 10px auto;
-        padding: 5px 10px;
-        color: blue;
-        border: 1px solid blue;
-    }
-
-</style>
+</script>
