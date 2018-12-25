19b8154475d97d5516774de841343797cedb7807
Ethan <372513646@qq.com>
2018-12-25 18:59:30 +0800

    add repaint & back

--- src/pages/add-exam/index.vue
+++ src/pages/add-exam/index.vue
@@ -6,9 +6,9 @@
                     @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend"></canvas>
             </div>
             <div class="buttons">
-                <button class="btn back"></button>
-                <button class="btn repaint"></button>
-                <button class="btn save"></button>
+                <button class="btn back" @click="back"></button>
+                <button class="btn repaint" @click="repaint"></button>
+                <button class="btn save" @click="save"></button>
             </div>
         </div>
     </fixed-view>
@@ -19,6 +19,7 @@
         getImageInfo,
         getNodeRect
     } from '@/utils'
+    const canvasId = 'addExam'
     export default {
         data() {
             return {
@@ -31,71 +32,96 @@
             fixedView
         },
         methods: {
+            back() {
+                const length = this.imgStack.length
+                if(length === 1) {
+                    return
+                }
+                this.imgStack.pop() // 当初当前显示的
+                const data = this.imgStack[length - 2];
+                this._useShot(data)
+            },
+            repaint() {
+                this.imgStack.length = 1
+                const data = this.imgStack[0]
+                this._useShot(data)
+            },
+            save() {
+
+            },
             async init() {
                 const data = await Promise.all([getImageInfo(this.src), getNodeRect('#canvas')])
                 const [imageInfo, canvasInfo] = data
                 const radio = imageInfo.width / imageInfo.height
                 this.canvasHeight = canvasInfo.width / radio
+                this.imgInfo = {
+                    width: canvasInfo.width,
+                    height: this.canvasHeight
+                }
                 this.context.drawImage(this.src, 0, 0, canvasInfo.width, this.canvasHeight)
-                this.context.draw()
+                this.context.draw(false, this._addShot)
             },
             touchstart(e) {
                 this.touch = {
                     startX: e.touches[0].x,
                     startY: e.touches[0].y
                 }
-                if (this.isClear) {
-                    this.context.setStrokeStyle('#F8F8F8')
-                    this.context.setLineCap('round')
-                    this.context.setLineJoin('round')
-                    this.context.setLineWidth(20)
-                    this.context.save()
-                    this.context.beginPath()
-                    this.context.arc(this.touch.startX, this.touch.startY, 5, 0, 2 * Math.PI, true)
-                    this.context.fill();
-                    this.context.restore();
-                } else {
-                    this.context.setStrokeStyle('#fff')
-                    this.context.setLineWidth(20)
-                    this.context.setLineCap('round') // 让线条圆润
-                    this.context.beginPath()
-                }
+                this.context.setStrokeStyle('#fff')
+                this.context.setLineWidth(20)
+                this.context.setLineCap('round') // 让线条圆润
+                this.context.beginPath()
             },
             touchmove(e) {
                 var startX1 = e.touches[0].x
                 var startY1 = e.touches[0].y
-                if (this.isClear) { //判断是否启用的橡皮擦功能 ture表示清除 false表示画画
-                    this.context.save(); //保存当前坐标轴的缩放、旋转、平移信息
-                    this.context.moveTo(this.touch.startX, this.touch.startY); //把路径移动到画布中的指定点，但不创建线条
-                    this.context.lineTo(startX1, startY1); //添加一个新点，然后在画布中创建从该点到最后指定点的线条
-                    this.context.stroke(); //对当前路径进行描边
-                    this.context.restore() //恢复之前保存过的坐标轴的缩放、旋转、平移信息
-                    this.touch = {
-                        startX: startX1,
-                        startY: startY1
-                    }
-                } else {
-                    this.context.moveTo(this.touch.startX, this.touch.startY)
-                    this.context.lineTo(startX1, startY1)
-                    this.context.stroke()
-                    this.touch = {
-                        startX: startX1,
-                        startY: startY1
-                    }
+                this.context.moveTo(this.touch.startX, this.touch.startY)
+                this.context.lineTo(startX1, startY1)
+                this.context.stroke()
+                this.touch = {
+                    startX: startX1,
+                    startY: startY1
                 }
                 this.context.draw(true)
             },
             touchend(e) {
-
+                // 保存快照
+                this._addShot()
+            },
+            _addShot() {
+                wx.canvasGetImageData({
+                    canvasId: canvasId,
+                    x: 0,
+                    y: 0,
+                    width: this.imgInfo.width,
+                    height: this.canvasHeight,
+                    success: (res) => {
+                        this.imgStack.push(res.data)
+                    }
+                })
+            },
+            _useShot(data) {
+                wx.canvasPutImageData({
+                    canvasId: canvasId,
+                    x: 0,
+                    y: 0,
+                    width: this.imgInfo.width,
+                    height: this.imgInfo.height,
+                    data,
+                    success: (res) => {
+                        console.log(res)
+                    }
+                })
             }
         },
         mounted() {
             this.src = this.$root.$mp.query.src
-            this.context = wx.createCanvasContext('addExam')
+            this.context = wx.createCanvasContext(canvasId)
             this.init()
         },
         created() {
             this.touch = {}
+            this.imgStack = []
+            this.imgInfo = {}
         }
     }
 </script>
